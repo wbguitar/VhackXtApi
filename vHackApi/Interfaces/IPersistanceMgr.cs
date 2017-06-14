4a427a473ee7a3ca8155b6e4f4cbabbf6f48704f
wbguitar <betti.francesco@gmail.com>
2017-06-14 11:06:05 +0200

    Added IPersistanceMgr
    Implements API's buddy delegated to store information in some place of its choice. Since only one and only one of them is needed, a good idea might be to choose this guy from a Singleton ancestry

---
+++ vHackApi/Interfaces/IPersistanceMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace vHackApi.Interfaces
+{
+    public interface IPersistanceMgr
+    {
+        bool AddAttack(string iP, Attacks att);
+
+        IPs AddIp(IPs dbIp);
+
+        IPs GetIp(string iP);
+
+        bool IpExist(string iP);
+
+        IEnumerable<IPs> ScannableIps();
+
+        void Update();
+
+        bool UpdateIp(IPs ip);
+    }
+}
