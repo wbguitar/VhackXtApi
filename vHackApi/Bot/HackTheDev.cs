797552117fe6e834de49269c6d41a216f4b57e98
wbguitar <betti.francesco@gmail.com>
2017-06-14 22:36:16 +0200

    HackTheDev
    hacks the dev's ip on every turn

---
+++ vHackApi/Bot/HackTheDev.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using vHackApi.Api;
+using vHackApi.Console;
+using vHackApi.Interfaces;
+
+namespace vHackApi.Bot
+{
+    public class HackTheDev : AHackTimer<HackTheDev>
+    {
+        private HackTheDev() { }
+
+        //private static HackTheDev inst;
+        //public static HackTheDev Instance
+        //{
+        //    get
+        //    {
+        //        if (inst == null)
+        //            inst = new HackTheDev();
+
+        //        return inst;
+        //    }
+        //}
+
+        public override void Set(IConfig cfg, vhAPI api)
+        {
+            if (hackTimer != null)
+            {
+                hackTimer.Dispose();
+                hackTimer = null;
+            }
+
+            var console = api.getConsole();
+
+            hackTimer = new Timer(
+                async (o) =>
+                {
+                    try
+                    { var s = await console.AttackIp("127.0.0.1"); }
+                    catch (Exception e)
+                    {
+                        cfg.logger.Log(e.ToString());
+                    }
+                }
+                , null, TimeSpan.Zero, cfg.hackDevPolling);
+        }
+    }
+}
